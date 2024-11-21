615c6996bb093860c89152e1d9636c3b922b4f5a
Anna Yesypenko <annayesypenko@gmail.com>
2024-11-21 10:02:20 -0600

    add T_LR,T_RR, etc

--- slab_example.py
+++ slab_example.py
@@ -12,7 +12,7 @@ p = 10  # polynomial order for hps
 kh = 8  # kh parameter for PDO
 
 pdo         = PDO2d(c11=const(1.0),c22=const(1.0),c=const(-kh**2))
-box         = np.array([[0,0],[0.5,1.0]])
+box         = np.array([[0,0],[0.75,1.0]])
 geom        = BoxGeometry(box)
 
 # setting p = 2 will use FD discretization
@@ -25,17 +25,45 @@ relerr    = slab.solver.verify_discretization(kh)
 
 print ("\t kh = %5.2f, relerr= %5.2e" % (kh,relerr))
 
+#### in order to assemble the T system, there are properties
+#### T_LL, T_LR, T_RL, T_RR which return linear operators
+
+# here I am sampling its action
+rank  = 10
+
+Omega = np.random.rand(slab.I_R.shape[0],rank) 
+Y_LR  = slab.T_LR(Omega)
+
+Q     = np.linalg.qr(Y_LR)[0]
+B     = slab.T_LR.rmatmat(Q).T
+
+# as expected, there is slow decay in the spectrum of T_LR
+
+T_LR_full =  slab.T_LR(np.eye(slab.I_R.shape[0]))
+relerr = np.linalg.norm(Q @ B - T_LR_full,ord=2) / np.linalg.norm(T_LR_full,ord=2)
+
+print("\t Low rank approximation of T_LR of rank=%d has relerr=%5.2e" % (rank,relerr))
+
+###########################################################################################
+
 fig = plt.figure()
 ax  = fig.add_subplot()
 
 XX  = slab.solver.XX
-I_C = slab.solver.I_C
-I_X = slab.solver.I_X
+I_C = slab.I_C
+I_X = slab.I_X
+I_L = slab.I_L
+I_R = slab.I_R
 
-ax.scatter(XX[I_X,0],XX[I_X,1])
-ax.scatter(XX[I_C,0],XX[I_C,1])
+ax.scatter(XX[I_X,0],XX[I_X,1],label='I_X')
+ax.scatter(XX[I_C,0],XX[I_C,1],label='I_C')
+ax.scatter(XX[I_L,0],XX[I_L,1],label='I_L')
+ax.scatter(XX[I_R,0],XX[I_R,1],label='I_R')
+
 ax.set_aspect('equal','box')
 
+plt.legend()
 plt.show()
 
-#### in order to assemble the T system, there is a method T_XX_op
+
+
--- slab_subdomain.py
+++ slab_subdomain.py
@@ -1,6 +1,7 @@
 from hps.hps_multidomain   import HPSMultidomain
 from hps.fd_discretization import FDDiscretization
 from scipy.sparse.linalg   import LinearOperator
+import numpy as np
 
 class SlabSubdomain:
 
@@ -24,19 +25,55 @@ class SlabSubdomain:
 
     @property
     def I_C(self):
-        return self.solver.I_X
+        return self.solver.I_C
 
     @property
     def solver(self):
         return self._solver
 
     @property
-    def T_XX_op(self):
+    def I_L(self):
+
+        hmin    = np.max(self.XX[1] - self.XX[0])
+        Lbool   = self.XX[self.I_X,0] < self.solver.geom.bounds[0,0] + 0.1*hmin
+        return self.I_X[np.where(Lbool)[0]]
+
+    @property
+    def I_R(self):
+
+        hmin    = np.max(self.XX[1] - self.XX[0])
+        Lbool   = self.XX[self.I_X,0] > self.solver.geom.bounds[1,0] - 0.1*hmin
+        return self.I_X[np.where(Lbool)[0]]
+
+    @property
+    def T_LL(self):
+        I1 = np.intersect1d(self.I_L,self.I_X,return_indices=True)[2]
+        return self.get_TX1X2_op(I1,I1)
+    
+    @property
+    def T_LR(self):
+        I1 = np.intersect1d(self.I_L,self.I_X,return_indices=True)[2]
+        I2 = np.intersect1d(self.I_R,self.I_X,return_indices=True)[2]
+        return self.get_TX1X2_op(I1,I2)
+
+    @property
+    def T_RL(self):
+        I1 = np.intersect1d(self.I_R,self.I_X,return_indices=True)[2]
+        I2 = np.intersect1d(self.I_L,self.I_X,return_indices=True)[2]
+        return self.get_TX1X2_op(I1,I2)
+
+    @property
+    def T_RR(self):
+        I1 = np.intersect1d(self.I_R,self.I_X,return_indices=True)[2]
+        return self.get_TX1X2_op(I1,I1)
+    
+
+    def get_TX1X2_op(self,I1,I2):
         nX = self.solver.I_X.shape[0]
 
-        A_XX = self.solver.A_XX
-        A_XC = self.solver.A_XC
-        A_CX = self.solver.A_CX
+        A_X1X2 = self.solver.A_XX[I1][:,I2]
+        A_X1C = self.solver.A_XC[I1]
+        A_CX2 = self.solver.A_CX[:,I2]
         LU_CC= self.solver.solver_CC
 
         def matmat(v,transpose=False):
@@ -48,16 +85,16 @@ class SlabSubdomain:
 
             if (not transpose):
 
-                result = A_XX @ v_tmp
-                result -= A_XC @ LU_CC.matmat(A_CX @ v_tmp)
+                result = A_X1X2 @ v_tmp
+                result -= A_X1C @ LU_CC.matmat(A_CX2 @ v_tmp)
             else:
-                result = A_XX.T @ v_tmp
-                result -= A_CX.T @ LU_CC.rmatmat(A_XC.T @ v_tmp)
+                result = A_X1X2.T @ v_tmp
+                result -= A_CX2.T @ LU_CC.rmatmat(A_X1C.T @ v_tmp)
 
             if (v.ndim == 1):
                 result = result.flatten()
             return result
 
-        return LinearOperator(shape=(nX,nX),\
-            matvec = matmat, rmatvec = lambda x: matmat(v,transpose=True),\
-            matmat = matmat, rmatmat = lambda x: matmat(v,transpose=True))
+        return LinearOperator(shape=A_X1X2.shape,\
+            matvec = matmat, rmatvec = lambda v: matmat(v,transpose=True),\
+            matmat = matmat, rmatmat = lambda v: matmat(v,transpose=True))
