4ee8607ba665f0105cd3b627531eba67511dab7d
Anna Yesypenko <anna@oden.utexas.edu>
2024-11-22 11:44:42 -0600

    Scr, Scl for double slab

--- slab_example.py
+++ slab_example.py
@@ -7,17 +7,23 @@ from slab_subdomain        import SlabSubdomain
 from time import time
 from matplotlib import pyplot as plt
 
-a = 1/8 # mesh parameter for hps discretization
-p = 10  # polynomial order for hps
-kh = 8  # kh parameter for PDO
+a = 1/8  # mesh parameter for hps discretization
+p = 10   # polynomial order for hps
+kh = 8   # kh parameter for PDO
 
-pdo         = PDO2d(c11=const(1.0),c22=const(1.0),c=const(-kh**2))
-box         = np.array([[0,0],[0.75,1.0]])
-geom        = BoxGeometry(box)
+ndim = 2 # 3d capable -- I recommend p < 8 in 3d.
+
+if (ndim == 2):
+
+	pdo         = PDO2d(c11=const(1.0),c22=const(1.0),c=const(-kh**2))
+	box         = np.array([[0,0],[2,1.0]])
+	geom        = BoxGeometry(box)
+else:
+	pdo         = PDO3d(c11=const(1.0),c22=const(1.0),c33=const(1.0),c=const(-kh**2))
+	box         = np.array([[0,0,0],[2,1.0,1.0]])
+	geom        = BoxGeometry(box)
 
 # setting p = 2 will use FD discretization
-# this code can also handle 3D discretizations, using a 3D geometry
-# see usage example in hps package
 slab      = SlabSubdomain(pdo,geom,a,p)
 
 # if you are using a constant coefficient PDE, you can verify discretization
@@ -28,37 +34,88 @@ print ("\t kh = %5.2f, relerr= %5.2e" % (kh,relerr))
 ###########################################################################################
 
 fig = plt.figure()
-ax  = fig.add_subplot()
-
 XX  = slab.solver.XX
 
-ax.scatter(XX[slab.Jx,0],XX[slab.Jx,1],label='Jx')
-ax.scatter(XX[slab.Ji,0],XX[slab.Ji,1],label='Ji')
-ax.scatter(XX[slab.Jl,0],XX[slab.Jl,1],label='Jl')
-ax.scatter(XX[slab.Jr,0],XX[slab.Jr,1],label='Jr')
+if (slab.solver.ndim == 2):
+	ax  = fig.add_subplot()
+	ax.scatter(XX[slab.Jx,0],XX[slab.Jx,1],label='Jx')
+	ax.scatter(XX[slab.Ji,0],XX[slab.Ji,1],label='Ji')
+	ax.scatter(XX[slab.Jl,0],XX[slab.Jl,1],label='Jl')
+	ax.scatter(XX[slab.Jr,0],XX[slab.Jr,1],label='Jr')
+	ax.scatter(XX[slab.Jc,0],XX[slab.Jc,1],label='Jc')
 
-ax.set_aspect('equal','box')
+	ax.set_aspect('equal','box')
+else:
+	ax  = fig.add_subplot(projection='3d')
+	ax.scatter(XX[slab.Jx,0],XX[slab.Jx,1],XX[slab.Jx,2],label='Jx')
+	ax.scatter(XX[slab.Ji,0],XX[slab.Ji,1],XX[slab.Ji,2],label='Ji')
+	ax.scatter(XX[slab.Jl,0],XX[slab.Jl,1],XX[slab.Jl,2],label='Jl')
+	ax.scatter(XX[slab.Jr,0],XX[slab.Jr,1],XX[slab.Jr,2],label='Jr')
+	ax.scatter(XX[slab.Jc,0],XX[slab.Jc,1],XX[slab.Jc,2],label='Jc')
+
+	ax.set_box_aspect([1,1,1])
 
 plt.legend()
 plt.show()
 
 ###########################################################################################
 
+def randQB(Aop,rank):
+
+	Omega = np.random.rand(Aop.shape[1],rank) 
+	Y     = Aop(Omega)
+
+	Q     = np.linalg.qr(Y)[0]
+	B     = Aop.rmatmat(Q).T
+	return Q,B
+
+###########################################################################################
+
 #### in order to assemble the T system, there are properties
-#### T_LL, T_LR, T_RL, T_RR which return linear operators
+#### Tll, Tlr, Trl, Trr which return linear operators
 
-# here I am sampling its action
+# here I am sampling the action of Tlr and confirming that it is low rank
 rank  = 10
+Q,B   = randQB(slab.Tlr,rank)
 
-Omega = np.random.rand(slab.Jr.shape[0],rank) 
-Y_LR  = slab.T_LR(Omega)
+Ifull  = np.eye(slab.Jl.shape[0])
 
-Q     = np.linalg.qr(Y_LR)[0]
-B     = slab.T_LR.rmatmat(Q).T
+Tlr    =  slab.Tlr(Ifull)
+relerr = np.linalg.norm(Q @ B - Tlr,ord=2) / np.linalg.norm(Tlr,ord=2)
+print("\t Low rank approximation of Tlr of rank=%d has relerr=%5.2e" % (rank,relerr))
 
-# as expected, there is slow decay in the spectrum of T_LR
 
-T_LR_full =  slab.T_LR(np.eye(slab.Jr.shape[0]))
-relerr = np.linalg.norm(Q @ B - T_LR_full,ord=2) / np.linalg.norm(T_LR_full,ord=2)
+# here I am printing information about the condition number of the T sub-matrix
+Ifull  = np.eye(slab.Jl.shape[0])
+Tll    = slab.Tll(Ifull)
+Tlr    = slab.Tlr(Ifull)
+Trl    = slab.Trl(Ifull)
+Trr    = slab.Trr(Ifull)
 
-print("\t Low rank approximation of T_LR of rank=%d has relerr=%5.2e" % (rank,relerr))
+Tsubmat= np.block([[Tll,Tlr],[Trl,Trr]])
+print("\t Condition number of Tsubmatrix %5.2f" % np.linalg.cond(Tsubmat))
+
+print("")
+###########################################################################################
+
+#### in order to assemble the S system, there are properties
+#### Scl, Scr which return linear operators
+#### NOTE that these operators "include" the negative sign
+
+# here I am sampling the action of Scr and confirming that it is low rank
+rank  = 10
+Q,B   = randQB(slab.Scr,rank)
+
+Scr_full =  slab.Scr(np.eye(slab.Jr.shape[0]))
+relerr   = np.linalg.norm(Q @ B - Scr_full,ord=2) / np.linalg.norm(Scr_full,ord=2)
+print("\t Low rank approximation of Scr of rank=%d has relerr=%5.2e" % (rank,relerr))
+
+# here I am printing information about the condition number of the S sub-matrix
+Ifull   = np.eye(slab.Jl.shape[0])
+Scl     = slab.Scl(Ifull)
+Scr     = slab.Scr(Ifull)
+
+Zfull   = np.zeros(Ifull.shape)
+
+Ssubmat = np.block(([[Ifull,Zfull,Zfull],[Scl,Ifull,Scr],[Zfull,Zfull,Ifull]]))
+print("\t Condition number of Ssubmatrix %5.2f" % np.linalg.cond(Ssubmat))
--- slab_subdomain.py
+++ slab_subdomain.py
@@ -8,12 +8,21 @@ def find_subindices_location(Jsub,Jlong):
 
 class SlabSubdomain:
 
-    def __init__(self,pdo,geom,a,p):
+    def __init__(self,pdo,geom,a,p,double_slab=True):
 
         if (p > 2):
             solver    = HPSMultidomain(pdo,geom,a,p)
+
+            npan_x    = solver.npan_dim[0]
+            if (double_slab and np.mod(npan_x,2) == 1 ):
+                raise ValueError("expected an even number of panels in x direction for double_slab")
         else:
             solver    = FDDiscretization(pdo,geom,a)
+
+            npoints_x = solver.npoints_dim[0]
+            if (double_slab and np.mod(npoints_x,2)== 0):
+                raise ValueError("expected an odd number of points in x direction for double_slab")
+
         self._solver  = solver
         self._solver.setup_solver_Aii()
 
@@ -32,13 +41,24 @@ class SlabSubdomain:
     # exterior points
     @property
     def Jx(self):
-
         return self.solver.Jx
 
     # interior points
     @property
     def Ji(self):
         return self.solver.Ji
+
+    # the `cut points' on the interior of a double slab
+    @property
+    def Jc(self):
+
+        len0         = self.solver.geom.bounds[1,0] - self.solver.geom.bounds[0,0]
+        center_bound = self.solver.geom.bounds[0,0] + 0.5*len0
+        hmin         = np.max(self.XX[1] - self.XX[0])
+
+        Lbool   = self.XX[self.Ji,0] < center_bound + 0.1*hmin
+        Rbool   = self.XX[self.Ji,0] > center_bound - 0.1*hmin
+        return self.Ji[np.where(np.logical_and(Lbool,Rbool))[0]]
 
     # the left exterior points
     @property
@@ -60,30 +80,44 @@ class SlabSubdomain:
     # linear algebra sparse operators
 
     @property
-    def T_LL(self):
+    def Tll(self):
         I1 = find_subindices_location(self.Jl,self.Jx)
         return self.get_TX1X2_op(I1,I1)
     
     @property
-    def T_LR(self):
+    def Tlr(self):
         I1 = find_subindices_location(self.Jl,self.Jx)
         I2 = find_subindices_location(self.Jr,self.Jx)
         return self.get_TX1X2_op(I1,I2)
 
     @property
-    def T_RL(self):
+    def Trl(self):
         I1 = find_subindices_location(self.Jr,self.Jx)
         I2 = find_subindices_location(self.Jl,self.Jx)
         return self.get_TX1X2_op(I1,I2)
 
     @property
-    def T_RR(self):
+    def Trr(self):
         I1 = find_subindices_location(self.Jr,self.Jx)
         return self.get_TX1X2_op(I1,I1)
     
+    # given Dirichlet data on left boundary,
+    # return consistent solution on cut boundary
+    @property
+    def Scl(self):
+        I1 = find_subindices_location(self.Jc,self.Ji)
+        I2 = find_subindices_location(self.Jl,self.Jx)
+        return self.get_SI1X2_op(I1,I2)
+
+    # given Dirichlet data on right boundary,
+    # return consistent solution on cut boundary
+    @property
+    def Scr(self):
+        I1 = find_subindices_location(self.Jc,self.Ji)
+        I2 = find_subindices_location(self.Jr,self.Jx)
+        return self.get_SI1X2_op(I1,I2)
 
     def get_TX1X2_op(self,I1,I2):
-        nX = self.solver.Jx.shape[0]
 
         Ax1x2 = self.solver.Axx[I1][:,I2]
         Ax1i  = self.solver.Axi[I1]
@@ -110,5 +144,38 @@ class SlabSubdomain:
             return result
 
         return LinearOperator(shape=Ax1x2.shape,\
+            matvec = matmat, rmatvec = lambda v: matmat(v,transpose=True),\
+            matmat = matmat, rmatmat = lambda v: matmat(v,transpose=True))
+
+    def get_SI1X2_op(self,I1,I2):
+
+        Aix2  = self.solver.Aix[:,I2]
+        LUii  = self.solver.solver_Aii
+
+        def matmat(v,transpose=False):
+
+            if (v.ndim == 1):
+                v_tmp = v[:,np.newaxis]
+            else:
+                v_tmp = v
+
+            if (not transpose):
+
+                tmp    = - LUii.matmat(Aix2 @ v_tmp)
+                result = tmp[I1]
+
+            else:
+
+                tmp    = np.zeros((self.Ji.shape[0],v_tmp.shape[-1]))
+                tmp[I1]= v_tmp 
+
+                result = - LUii.rmatmat(tmp)
+                result = Aix2.T @ result
+
+            if (v.ndim == 1):
+                result = result.flatten()
+            return result
+
+        return LinearOperator(shape=(I1.shape[0],I2.shape[0]),\
             matvec = matmat, rmatvec = lambda v: matmat(v,transpose=True),\
             matmat = matmat, rmatmat = lambda v: matmat(v,transpose=True))
