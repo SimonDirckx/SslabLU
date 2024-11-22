f0c3de5d50b29a74aa588e91130b3ed34ed81388
Anna Yesypenko <anna@oden.utexas.edu>
2024-11-22 10:24:31 -0600

    minor edits to index notation

--- slab_example.py
+++ slab_example.py
@@ -25,13 +25,32 @@ relerr    = slab.solver.verify_discretization(kh)
 
 print ("\t kh = %5.2f, relerr= %5.2e" % (kh,relerr))
 
+###########################################################################################
+
+fig = plt.figure()
+ax  = fig.add_subplot()
+
+XX  = slab.solver.XX
+
+ax.scatter(XX[slab.Jx,0],XX[slab.Jx,1],label='Jx')
+ax.scatter(XX[slab.Ji,0],XX[slab.Ji,1],label='Ji')
+ax.scatter(XX[slab.Jl,0],XX[slab.Jl,1],label='Jl')
+ax.scatter(XX[slab.Jr,0],XX[slab.Jr,1],label='Jr')
+
+ax.set_aspect('equal','box')
+
+plt.legend()
+plt.show()
+
+###########################################################################################
+
 #### in order to assemble the T system, there are properties
 #### T_LL, T_LR, T_RL, T_RR which return linear operators
 
 # here I am sampling its action
 rank  = 10
 
-Omega = np.random.rand(slab.I_R.shape[0],rank) 
+Omega = np.random.rand(slab.Jr.shape[0],rank) 
 Y_LR  = slab.T_LR(Omega)
 
 Q     = np.linalg.qr(Y_LR)[0]
@@ -39,31 +58,7 @@ B     = slab.T_LR.rmatmat(Q).T
 
 # as expected, there is slow decay in the spectrum of T_LR
 
-T_LR_full =  slab.T_LR(np.eye(slab.I_R.shape[0]))
+T_LR_full =  slab.T_LR(np.eye(slab.Jr.shape[0]))
 relerr = np.linalg.norm(Q @ B - T_LR_full,ord=2) / np.linalg.norm(T_LR_full,ord=2)
 
 print("\t Low rank approximation of T_LR of rank=%d has relerr=%5.2e" % (rank,relerr))
-
-###########################################################################################
-
-fig = plt.figure()
-ax  = fig.add_subplot()
-
-XX  = slab.solver.XX
-I_C = slab.I_C
-I_X = slab.I_X
-I_L = slab.I_L
-I_R = slab.I_R
-
-ax.scatter(XX[I_X,0],XX[I_X,1],label='I_X')
-ax.scatter(XX[I_C,0],XX[I_C,1],label='I_C')
-ax.scatter(XX[I_L,0],XX[I_L,1],label='I_L')
-ax.scatter(XX[I_R,0],XX[I_R,1],label='I_R')
-
-ax.set_aspect('equal','box')
-
-plt.legend()
-plt.show()
-
-
-
--- slab_subdomain.py
+++ slab_subdomain.py
@@ -3,6 +3,9 @@ from hps.fd_discretization import FDDiscretization
 from scipy.sparse.linalg   import LinearOperator
 import numpy as np
 
+def find_subindices_location(Jsub,Jlong):
+    return np.intersect1d(Jsub,Jlong,return_indices=True)[2]
+
 class SlabSubdomain:
 
     def __init__(self,pdo,geom,a,p):
@@ -12,69 +15,80 @@ class SlabSubdomain:
         else:
             solver    = FDDiscretization(pdo,geom,a)
         self._solver  = solver
-
-        self._solver.setup_solver_CC()
+        self._solver.setup_solver_Aii()
 
     @property
     def XX(self):
         return self.solver.XX
 
     @property
-    def I_X(self):
-        return self.solver.I_X
-
-    @property
-    def I_C(self):
-        return self.solver.I_C
-
-    @property
     def solver(self):
         return self._solver
 
+
+    ##########################################################################
+    # index vectors
+
+    # exterior points
     @property
-    def I_L(self):
+    def Jx(self):
+
+        return self.solver.Jx
+
+    # interior points
+    @property
+    def Ji(self):
+        return self.solver.Ji
+
+    # the left exterior points
+    @property
+    def Jl(self):
 
         hmin    = np.max(self.XX[1] - self.XX[0])
-        Lbool   = self.XX[self.I_X,0] < self.solver.geom.bounds[0,0] + 0.1*hmin
-        return self.I_X[np.where(Lbool)[0]]
+        Lbool   = self.XX[self.Jx,0] < self.solver.geom.bounds[0,0] + 0.1*hmin
+        return self.Jx[np.where(Lbool)[0]]
 
+    # the right exterior points
     @property
-    def I_R(self):
+    def Jr(self):
 
         hmin    = np.max(self.XX[1] - self.XX[0])
-        Lbool   = self.XX[self.I_X,0] > self.solver.geom.bounds[1,0] - 0.1*hmin
-        return self.I_X[np.where(Lbool)[0]]
+        Lbool   = self.XX[self.Jx,0] > self.solver.geom.bounds[1,0] - 0.1*hmin
+        return self.Jx[np.where(Lbool)[0]]
+
+    ############################################################################
+    # linear algebra sparse operators
 
     @property
     def T_LL(self):
-        I1 = np.intersect1d(self.I_L,self.I_X,return_indices=True)[2]
+        I1 = find_subindices_location(self.Jl,self.Jx)
         return self.get_TX1X2_op(I1,I1)
     
     @property
     def T_LR(self):
-        I1 = np.intersect1d(self.I_L,self.I_X,return_indices=True)[2]
-        I2 = np.intersect1d(self.I_R,self.I_X,return_indices=True)[2]
+        I1 = find_subindices_location(self.Jl,self.Jx)
+        I2 = find_subindices_location(self.Jr,self.Jx)
         return self.get_TX1X2_op(I1,I2)
 
     @property
     def T_RL(self):
-        I1 = np.intersect1d(self.I_R,self.I_X,return_indices=True)[2]
-        I2 = np.intersect1d(self.I_L,self.I_X,return_indices=True)[2]
+        I1 = find_subindices_location(self.Jr,self.Jx)
+        I2 = find_subindices_location(self.Jl,self.Jx)
         return self.get_TX1X2_op(I1,I2)
 
     @property
     def T_RR(self):
-        I1 = np.intersect1d(self.I_R,self.I_X,return_indices=True)[2]
+        I1 = find_subindices_location(self.Jr,self.Jx)
         return self.get_TX1X2_op(I1,I1)
     
 
     def get_TX1X2_op(self,I1,I2):
-        nX = self.solver.I_X.shape[0]
+        nX = self.solver.Jx.shape[0]
 
-        A_X1X2 = self.solver.A_XX[I1][:,I2]
-        A_X1C = self.solver.A_XC[I1]
-        A_CX2 = self.solver.A_CX[:,I2]
-        LU_CC= self.solver.solver_CC
+        Ax1x2 = self.solver.Axx[I1][:,I2]
+        Ax1i  = self.solver.Axi[I1]
+        Aix2  = self.solver.Aix[:,I2]
+        LUii  = self.solver.solver_Aii
 
         def matmat(v,transpose=False):
 
@@ -85,16 +99,16 @@ class SlabSubdomain:
 
             if (not transpose):
 
-                result = A_X1X2 @ v_tmp
-                result -= A_X1C @ LU_CC.matmat(A_CX2 @ v_tmp)
+                result = Ax1x2 @ v_tmp
+                result -= Ax1i @ LUii.matmat(Aix2 @ v_tmp)
             else:
-                result = A_X1X2.T @ v_tmp
-                result -= A_CX2.T @ LU_CC.rmatmat(A_X1C.T @ v_tmp)
+                result = Ax1x2.T @ v_tmp
+                result -= Aix2.T @ LUii.rmatmat(Ax1i.T @ v_tmp)
 
             if (v.ndim == 1):
                 result = result.flatten()
             return result
 
-        return LinearOperator(shape=A_X1X2.shape,\
+        return LinearOperator(shape=Ax1x2.shape,\
             matvec = matmat, rmatvec = lambda v: matmat(v,transpose=True),\
             matmat = matmat, rmatmat = lambda v: matmat(v,transpose=True))
