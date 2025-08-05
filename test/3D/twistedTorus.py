3389260b4542d1d30568fd672ba7c7d428ee0e08
SimonDirckx <simondirckx@hotmail.com>
2025-08-05 16:57:45 -0500

    added twisted torus transforms

--- test/3D/twistedTorus.py
+++ test/3D/twistedTorus.py
@@ -1,311 +1,143 @@
 import numpy as np
 import pdo.pdo as pdo
 import solver.solver as solverWrap
-#import multiSlab as MS
-#import matAssembly.matAssembler as mA
 import matplotlib.pyplot as plt
-#import geometry.standardGeometries as stdGeom
-#import geometry.skeleton as skelTon
-#import time
-#import hps.hps_multidomain as HPS
-#import hps.geom as hpsGeom
-#from scipy.sparse        import block_diag
-#import scipy.sparse as sparse
-#import scipy.sparse.linalg as splinalg
-#from scipy import interpolate
-#from scipy.interpolate import griddata
-#from scipy.sparse.linalg   import LinearOperator
-#from scipy.sparse.linalg import gmres
-#from solver.solver import stMap
-#import matAssembly.matAssembler as mA
-#from matplotlib.patches import Polygon
-#from hps.geom              import BoxGeometry, ParametrizedGeometry2D,ParametrizedGeometry3D
-#import matplotlib as mpl
-#from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy.spatial import Delaunay
 from matplotlib import cm
 import matplotlib.tri as tri
+import twistedTorusGeometry as twisted
+import jax.numpy as jnp
+import multislab.oms as oms
+import matAssembly.matAssembler as mA
+import scipy
+from packaging.version import Version
+
+# validation&testing
+import time
+from scipy.sparse.linalg import gmres
+#import solver.HPSInterp3D as interp
+import matplotlib.pyplot as plt
+
+class gmres_info(object):
+    def __init__(self, disp=False):
+        self._disp = disp
+        self.niter = 0
+        self.resList=[]
+    def __call__(self, rk=None):
+        self.niter += 1
+        self.resList+=[rk]
+        if self._disp:
+            print('iter %3i\trk = %s' % (self.niter, str(rk)))
+
+nwaves = 1.24
+wavelength = 4/nwaves
+kh = (nwaves/4)*2.*np.pi
+
+
+def bfield(p,kh):
+    return -kh*kh*jnp.ones_like(p[...,0])
+
+def bc(p):
+    z=twisted.z1(p,True)
+    return np.sin(kh*z)
+
+param_geom = twisted.param_geom(True)
+pdo_mod = param_geom.transform_helmholtz_pdo(bfield,kh)
+
+
+H = 1./8.
+N = (int)(1./H)
+slabs = []
+for n in range(N):
+    bnds_n = [[n*H,0.,0.],[(n+1)*H,1.,1.]]
+    slabs+=[bnds_n]
+
+connectivity = [[N-1,0]]
+for i in range(N-1):
+    connectivity+=[[i,i+1]]
+if_connectivity = []
+for i in range(N):
+    if_connectivity+=[[(i-1)%N,(i+1)%N]]
+
+period = 1.
+p = 10
+a = [H/8.,1/16,1/16]
+assembler = mA.rkHMatAssembler(p*p,100)
+opts = solverWrap.solverOptions('hps',[p,p,p],a)
 
 '''
-def z1(p):
-    c=np.cos(np.pi*p[1])
-    s=np.sin(np.pi*p[1])
-    rot = np.array([[c,-s],[s,c]])
-    q = rot@np.array([p[0],p[2]])
-    return c*(R+1+q[0])
+OMS = oms.oms(slabs,pdo_mod,lambda p:twisted.gb(p,True),opts,connectivity,if_connectivity,1.)
+print("computing Stot & rhstot...")
+Stot,rhstot = OMS.construct_Stot_and_rhstot(bc,assembler,2)
+print("done")
 
-def z2(p):
-    c=np.cos(np.pi*p[1])
-    s=np.sin(np.pi*p[1])
-    rot = np.array([[c,-s],[s,c]])
-    q = rot@np.array([p[0],p[2]])
-    return s*(R+1+q[0])
-def z3(p):
-    c=np.cos(np.pi*p[1])
-    s=np.sin(np.pi*p[1])
-    rot = np.array([[c,-s],[s,c]])
-    q = rot@np.array([p[0],p[2]])
-    return q[1]
+
+gInfo = gmres_info()
+stol = 1e-10*H*H
+
+if Version(scipy.__version__)>=Version("1.14"):
+    uhat,info   = gmres(Stot,rhstot,rtol=stol,callback=gInfo,maxiter=500,restart=500)
+else:
+    uhat,info   = gmres(Stot,rhstot,tol=stol,callback=gInfo,maxiter=500,restart=500)
+
+stop_solve = time.time()
+res = Stot@uhat-rhstot
+
+
+print("=============SUMMARY==============")
+print("H                        = ",'%10.3E'%H)
+print("ord                      = ",p)
+print("L2 rel. res              = ", np.linalg.norm(res)/np.linalg.norm(rhstot))
+print("GMRES iters              = ", gInfo.niter)
+print("==================================")
+
+uitot = np.zeros(shape=(0,))
+XXtot = np.zeros(shape=(0,3))
+dofs = 0
+glob_target_dofs=OMS.glob_target_dofs
+glob_source_dofs=OMS.glob_source_dofs
+nc = OMS.nc
+del OMS
+
+
+
+
+
+
+
+
+# check err.
+print("uhat shape = ",uhat.shape)
+print("uhat type = ",type(uhat))
+print("nc = ",nc)
 '''
-
-def z1(p):
-    c=np.cos(np.pi*p[:,1])
-    s=np.sin(np.pi*p[:,1])
-    c2 = np.multiply(c,c)
-    cs = np.multiply(c,s)
-    q = np.multiply(c2,p[:,0])-np.multiply(cs,p[:,2])+c*(R+1)
-    return q
-
-def z2(p):
-    c=np.cos(np.pi*p[:,1])
-    s=np.sin(np.pi*p[:,1])
-    s2 = np.multiply(s,s)
-    cs = np.multiply(c,s)
-    q = np.multiply(cs,p[:,0])-np.multiply(s2,p[:,2])+s*(R+1)
-    return q
-def z3(p):
-    c=np.cos(np.pi*p[:,1])
-    s=np.sin(np.pi*p[:,1])
-    q = np.multiply(s,p[:,0])+np.multiply(c,p[:,2])
-    return q
-
-
-def y1(p):
-    # p is a vector of points, Nx3
-    th = np.arctan2(p[:,1],p[:,0])
-    c=np.cos(th)
-    s=np.sin(th)
-    c2 = np.multiply(c,c)
-    cs = np.multiply(c,s)
-    q = np.multiply(p[:,0],c2)+np.multiply(p[:,1],cs)-(R+1)*c + np.multiply(s,p[:,2])
-    return q
-
-def y2(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    return th/np.pi
-
-def y3(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    c=np.cos(th)
-    s=np.sin(th)
-    s2 = np.multiply(s,s)
-    cs = np.multiply(c,s)
-    q = -np.multiply(p[:,0],cs)-np.multiply(p[:,1],s2)+(R+1)*s+np.multiply(c,p[:,2])
-    return q
-
-def y1_d1(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    c2t = np.cos(2*th)
-    s2t = np.sin(2*th)
-    s   = np.sin(th)
-    c   = np.cos(th)
-    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    A   = (s2t*p[:,0]*p[:,1] - c2t*p[:,1]*p[:,1] - (R+1)*s*p[:,1] - p[:,2]*p[:,1]*c)
-    return (c2t+1)/2. + A/r2
-
-def y1_d2(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    c2t = np.cos(2*th)
-    s2t = np.sin(2*th)
-    s   = np.sin(th)
-    c   = np.cos(th)
-    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    A   = ( c2t*p[:,0]*p[:,1] - s2t*p[:,0]*p[:,0] + (R+1)*s*p[:,0] + p[:,2]*p[:,0]*c)
-    return s2t/2. +A/r2
-
-def y1_d3(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    return np.sin(th)
-
-
-def y2_d1(p):
-    r2 = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    return -(p[:,1]/r2)/np.pi
-
-def y2_d2(p):
-    r2 = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    return (p[:,0]/r2)/np.pi
-
-
-def y3_d1(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    c2t = np.cos(2*th)
-    s2t = np.sin(2*th)
-    s   = np.sin(th)
-    c   = np.cos(th)
-    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    A   = (c2t*p[:,0]*p[:,1] + s2t*p[:,0]*p[:,0] - (R+1)*c*p[:,1] + p[:,2]*p[:,1]*s)
-    return -s2t/2. + A/r2
-
-def y3_d2(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    c2t = np.cos(2*th)
-    s2t = np.sin(2*th)
-    s   = np.sin(th)
-    c   = np.cos(th)
-    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    A   = (s2t*p[:,0]*p[:,1] + c2t*p[:,0]*p[:,0] - (R+1)*c*p[:,0] + p[:,2]*p[:,0]*s)
-    return (c2t-1)/2. - A/r2
-
-def y3_d3(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    return np.cos(th)
-
-
-def y1_d1d1(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    c2t = np.cos(2*th)
-    s2t = np.sin(2*th)
-    s   = np.sin(th)
-    c   = np.cos(th)
-    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    A   = (s2t*p[:,0]*p[:,1] - c2t*p[:,1]*p[:,1] - (R+1)*s*p[:,1] - p[:,2]*p[:,1]*c)
-    dA = p[:,1]*s2t-(2*c2t*p[:,0]*p[:,1]**2+2*s2t*p[:,1]**3-(R+1)*c*p[:,1]**2 + p[:,2]*s*p[:,1]**2 )/r2
-    return (p[:,1]*s2t + dA - 2*A*p[:,0]/r2 )/r2
-
-def y1_d2d2(p):
-    th = np.arctan2(p[:,1],p[:,0])
-    c2t = np.cos(2*th)
-    s2t = np.sin(2*th)
-    s   = np.sin(th)
-    c   = np.cos(th)
-    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    A   = ( c2t*p[:,0]*p[:,1] - s2t*p[:,0]*p[:,0] + (R+1)*s*p[:,0] + p[:,2]*p[:,0]*c)
-    dA  = p[:,0]*c2t-(2*s2t*p[:,1]*p[:,0]**2+2*p[:,1]*s2t*p[:,0]**3-(R+1)*c*p[:,0]**2+p[:,2]*s*p[:,0]**2)/r2
-    return (c2t*p[:,0] + dA - 2*A*p[:,1]/r2 )/r2
-
-def y2_d1d1(p):
-    r2 = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    return (2*p[:,1]*p[:,0]/r2)/(r2*np.pi)
-
-def y2_d2d2(p):
-    r2 = p[:,0]*p[:,0]+p[:,1]*p[:,1]
-    return -2*(p[:,0]*p[:,1]/r2)/(r2*np.pi)
-
-
-
-n=100
-R = 1.5
-xpts = np.linspace(-1,1,n)
-ypts = np.linspace(-1,1,4*n)
-zpts = np.linspace(-1,1,n)
-nx = len(xpts)
-ny = len(ypts)
-nz = len(zpts)
-
-def gb(p):
-    return np.abs(p[0]+1)<1e-10 or np.abs(p[0]-1)<1e-10 or np.abs(p[2]+1)<1e-10 or np.abs(p[2]-1)<1e-10
-def gbu(p):
-    return np.abs(p[2]-1)<1e-10
-def gbd(p):
-    return np.abs(p[2]+1)<1e-10
-def gbl(p):
-    return np.abs(p[0]+1)<1e-10
-def gbr(p):
-    return np.abs(p[0]-1)<1e-10
-
-XXu= np.zeros(shape=(0,3))
-XXd= np.zeros(shape=(0,3))
-XXl= np.zeros(shape=(0,3))
-XXr= np.zeros(shape=(0,3))
-
-for i in range(nx):
-    for j in range(ny):
-        for k in range(nz):
-            p=np.array([xpts[i],ypts[j],zpts[k]])
-            if gbu(p):
-                XXu=np.append(XXu,np.reshape(p,newshape=(1,3)),axis=0)
-            if gbd(p):
-                XXd=np.append(XXd,np.reshape(p,newshape=(1,3)),axis=0)
-            if gbl(p):
-                XXl=np.append(XXl,np.reshape(p,newshape=(1,3)),axis=0)
-            if gbr(p):
-                XXr=np.append(XXr,np.reshape(p,newshape=(1,3)),axis=0)
-
-triu = Delaunay(XXu[:,0:2]).simplices
-trid = Delaunay(XXd[:,0:2]).simplices
-tril = Delaunay(XXl[:,1:3]).simplices
-trir = Delaunay(XXr[:,1:3]).simplices
-
-
-tritot = triu
-tritot = np.append(tritot,XXu.shape[0]+trid,axis=0)
-tritot = np.append(tritot,XXu.shape[0]+XXd.shape[0]+tril,axis=0)
-tritot = np.append(tritot,XXu.shape[0]+XXd.shape[0]+XXl.shape[0]+trir,axis=0)
-
-XXtot = np.append(XXu,XXd,axis=0)
-XXtot = np.append(XXtot,XXl,axis=0)
-XXtot = np.append(XXtot,XXr,axis=0)
-
-print('XXtot shape = ',XXtot.shape)
-
-ZZtotx = z1(XXtot)
-ZZtoty = z2(XXtot)
-ZZtotz = z3(XXtot)
-
-ZZtot = np.zeros(shape=(ZZtotx.shape[0],3))
-ZZtot[:,0]=ZZtotx
-ZZtot[:,1]=ZZtoty
-ZZtot[:,2]=ZZtotz
-
-n=800
-xcross = np.linspace(-(R+2),(R+2),n)
-ycross = np.linspace(-(R+2),(R+2),n)
-h = 4*(2*(R+2)/n)*np.sqrt(2)
-cross = np.zeros(shape=(0,2))
-xy = np.zeros(shape = (len(xcross)*len(ycross),3))
-for i in range(len(xcross)):
-    for j in range(len(ycross)):
-        xy[i+j*len(xcross),:] = [xcross[i],ycross[j],0.]
-
-YYcross = np.zeros(shape=xy.shape)
-YYcross[:,0] = y1(xy)
-YYcross[:,1] = y2(xy)
-YYcross[:,2] = y3(xy)
-
-for ij in range(YYcross.shape[0]):
-    yy = YYcross[ij,:]
-    if yy[0]>-1 and yy[0]<1 and yy[1]>-1 and yy[1]<1 and yy[2]>-1 and yy[2]<1:
-        cross = np.append(cross,np.reshape(xy[ij,0:2],newshape=(1,2)),axis=0)
-
-
-tricross = tri.Triangulation(cross[:,0],cross[:,1])
-max_radius = h
-triangles = tricross.triangles
-
-# Mask off unwanted triangles.
-xtri = cross[triangles,0] - np.roll(cross[triangles,0], 1, axis=1)
-ytri = cross[triangles,1] - np.roll(cross[triangles,1], 1, axis=1)
-maxi = np.max(np.sqrt(xtri**2 + ytri**2), axis=1)
-tricross.set_mask(maxi > max_radius)
-
-YYtotx = y1(ZZtot)
-YYtoty = y2(ZZtot)
-YYtotz = y3(ZZtot)
-
-
-fig = plt.figure(0)
-ax = fig.add_subplot(projection='3d')
-ax.plot_trisurf(
-    ZZtot[:,0], ZZtot[:,1], ZZtot[:,2],
-    triangles=tritot,cmap = cm.viridis,edgecolor='None',antialiased=False,linewidth=0
-)
-plt.axis('equal')
-plt.axis('off')
-plt.savefig('twistedTorus.png',transparent=True,dpi=300)
 fig = plt.figure(1)
-ax = fig.add_subplot(projection='3d')
-ax.scatter(YYtotx, YYtoty, YYtotz)
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
-plt.axis('equal')
+N=len(connectivity)
+errInf = 0.
+for slabInd in range(len(connectivity)):
+    geom    = np.array(oms.join_geom(slabs[connectivity[slabInd][0]],slabs[connectivity[slabInd][1]],period))
+    slab_i  = oms.slab(geom,lambda p:twisted.gb(p,True))
+    solver  = oms.solverWrap.solverWrapper(opts)
+    solver.construct(geom,pdo_mod)
+    
+    Il,Ir,Ic,Igb,XXi,XXb = slab_i.compute_idxs_and_pts(solver)
 
-fig = plt.figure(2)
-ax = fig.add_subplot()
-#ax.tripcolor(tricross, np.ones(shape=(cross.shape[0],)), shading='gouraud')
-ax.tripcolor(tricross, np.cos(3*np.arctan2(cross[:,1],cross[:,0])),shading='gouraud')
-plt.axis('equal')
-plt.axis('off')
-plt.savefig('twistedTorusSection.png',transparent=True,dpi=300)
-plt.show()
+    startL = ((slabInd-1)%N)
+    startR = ((slabInd+1)%N)
+    #ul = uhat[startL*nc:(startL+1)*nc]
+    #ur = uhat[startR*nc:(startR+1)*nc]
+    u0l = bc(XXb[Il,:])
+    u0r = bc(XXb[Ir,:])
+    g = np.zeros(shape=(XXb.shape[0],))
+    g[Il]=bc(XXb[Il,:])
+    g[Ir]=bc(XXb[Ir,:])
+    g[Igb] = bc(XXb[Igb,:])
+    g=g[:,np.newaxis]
+    uu = solver.solver.solve_dir_full(g)
+    uu0 = bc(solver.XXfull)
+    uu=uu.flatten()
+    errI=np.linalg.norm(uu-uu0,ord=np.inf)
+    errInf = np.max([errInf,errI])
+    print(errI)
+print("sup norm error = ",errInf)
+
---
+++ test/3D/twistedTorusGeometry.py
@@ -0,0 +1,471 @@
+
+import numpy as np
+import jax.numpy as jnp
+from hps.geom              import ParametrizedGeometry3D
+R = 1.5
+bnds = [[0.,0.,0.],[1.,1.,1.]]
+
+def z1_np(p):
+    c=np.cos(np.pi*p[:,1])
+    s=np.sin(np.pi*p[:,1])
+    c2 = np.multiply(c,c)
+    cs = np.multiply(c,s)
+    q = np.multiply(c2,p[:,0])-np.multiply(cs,p[:,2])+c*(R+1)
+    return q
+
+def z2_np(p):
+    c=np.cos(np.pi*p[:,1])
+    s=np.sin(np.pi*p[:,1])
+    s2 = np.multiply(s,s)
+    cs = np.multiply(c,s)
+    q = np.multiply(cs,p[:,0])-np.multiply(s2,p[:,2])+s*(R+1)
+    return q
+def z3_np(p):
+    c=np.cos(np.pi*p[:,1])
+    s=np.sin(np.pi*p[:,1])
+    q = np.multiply(s,p[:,0])+np.multiply(c,p[:,2])
+    return q
+
+
+def y1_np(p):
+    # p is a vector of points, Nx3
+    th = np.arctan2(p[:,1],p[:,0])
+    c=np.cos(th)
+    s=np.sin(th)
+    c2 = np.multiply(c,c)
+    cs = np.multiply(c,s)
+    q = np.multiply(p[:,0],c2)+np.multiply(p[:,1],cs)-(R+1)*c + np.multiply(s,p[:,2])
+    return q
+
+def y2_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    return th/np.pi
+
+def y3_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    c=np.cos(th)
+    s=np.sin(th)
+    s2 = np.multiply(s,s)
+    cs = np.multiply(c,s)
+    q = -np.multiply(p[:,0],cs)-np.multiply(p[:,1],s2)+(R+1)*s+np.multiply(c,p[:,2])
+    return q
+
+#verified
+def y1_d1_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    c2t = np.cos(2*th)
+    s2t = np.sin(2*th)
+    s   = np.sin(th)
+    c   = np.cos(th)
+    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    A   = (s2t*p[:,0]*p[:,1] - c2t*p[:,1]**2 - (R+1)*s*p[:,1] - p[:,2]*p[:,1]*c)
+    return (c2t+1)/2. + A/r2
+
+#verified
+def y1_d2_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    c2t = np.cos(2*th)
+    s2t = np.sin(2*th)
+    s   = np.sin(th)
+    c   = np.cos(th)
+    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    A   = ( c2t*p[:,0]*p[:,1] - s2t*p[:,0]*p[:,0] + (R+1)*s*p[:,0] + p[:,2]*p[:,0]*c)
+    return s2t/2. + A/r2
+
+def y1_d3_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    return np.sin(th)
+
+#verified
+def y2_d1_np(p):
+    r2 = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    return -(p[:,1]/r2)/np.pi
+#verified
+def y2_d2_np(p):
+    r2 = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    return (p[:,0]/r2)/np.pi
+
+
+def y3_d1_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    c2t = np.cos(2*th)
+    s2t = np.sin(2*th)
+    s   = np.sin(th)
+    c   = np.cos(th)
+    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    A   = (c2t*p[:,0]*p[:,1] + s2t*p[:,1]**2 - (R+1)*c*p[:,1] + p[:,2]*p[:,1]*s)
+    return -s2t/2. + A/r2
+
+def y3_d2_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    c2t = np.cos(2*th)
+    s2t = np.sin(2*th)
+    s   = np.sin(th)
+    c   = np.cos(th)
+    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    A   = (s2t*p[:,0]*p[:,1] + c2t*p[:,0]*p[:,0] - (R+1)*c*p[:,0] + p[:,2]*p[:,0]*s)
+    return (c2t-1)/2. - A/r2
+#verified
+def y3_d3_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    return np.cos(th)
+
+#verified
+def y1_d1d1_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    c2t = np.cos(2*th)
+    s2t = np.sin(2*th)
+    s   = np.sin(th)
+    c   = np.cos(th)
+    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    A   = (s2t*p[:,0]*p[:,1] - c2t*p[:,1]*p[:,1] - (R+1)*s*p[:,1] - p[:,2]*p[:,1]*c)
+    dA = p[:,1]*s2t-(2*c2t*p[:,0]*(p[:,1]**2)+2*s2t*p[:,1]**3-(R+1)*c*p[:,1]**2 + p[:,2]*s*(p[:,1]**2) )/r2
+    return (p[:,1]*s2t + dA - 2*A*p[:,0]/r2 )/r2
+#verified
+def y1_d2d2_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    c2t = np.cos(2*th)
+    s2t = np.sin(2*th)
+    s   = np.sin(th)
+    c   = np.cos(th)
+    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    A   = ( c2t*p[:,0]*p[:,1] - s2t*p[:,0]*p[:,0] + (R+1)*s*p[:,0] + p[:,2]*p[:,0]*c)
+    dA  = p[:,0]*c2t-(2*s2t*p[:,1]*p[:,0]**2+2*c2t*(p[:,0]**3)-(R+1)*c*(p[:,0]**2)+p[:,2]*s*(p[:,0]**2))/r2
+    return (c2t*p[:,0] + dA - 2*A*p[:,1]/r2 )/r2
+#verified
+def y2_d1d1_np(p):
+    r2 = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    return (2*p[:,1]*p[:,0]/r2)/(r2*np.pi)
+#verified
+def y2_d2d2_np(p):
+    r2 = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    return -2*(p[:,0]*p[:,1]/r2)/(r2*np.pi)
+
+#verified
+def y3_d1d1_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    c2t = np.cos(2*th)
+    s2t = np.sin(2*th)
+    s   = np.sin(th)
+    c   = np.cos(th)
+    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    A   = (c2t*p[:,0]*p[:,1] + s2t*p[:,1]**2 - (R+1)*c*p[:,1] + p[:,2]*p[:,1]*s)
+    dA  = p[:,1]*c2t+(2*p[:,0]*(p[:,1]**2)*s2t-2*(p[:,1]**3)*c2t-(R+1)*(p[:,1]**2)*s-p[:,2]*(p[:,1]**2)*c)/r2
+    return (c2t*p[:,1] + dA - 2*p[:,0]*A/r2)/r2
+#verified
+def y3_d2d2_np(p):
+    th = np.arctan2(p[:,1],p[:,0])
+    c2t = np.cos(2*th)
+    s2t = np.sin(2*th)
+    s   = np.sin(th)
+    c   = np.cos(th)
+    r2  = p[:,0]*p[:,0]+p[:,1]*p[:,1]
+    A   = (s2t*p[:,0]*p[:,1] + c2t*p[:,0]*p[:,0] - (R+1)*c*p[:,0] + p[:,2]*p[:,0]*s)
+    dA  = p[:,0]*s2t+(2*(p[:,0]**2)*p[:,1]*c2t-2*(p[:,0]**3)*s2t + (R+1)*(p[:,0]**2)*s + p[:,2]*(p[:,0]**2)*c)/r2
+    return -(s2t*p[:,0] + dA/r2 - 2*p[:,1]*A/r2)/r2
+def gb_np(p):
+    return ((np.abs(p[:,1]-bnds[0][1]))<1e-14) | ((np.abs(p[:,1]-bnds[1][1]))<1e-14) | (np.abs(p[:,2]-bnds[0][2])<1e-14) | (np.abs(p[:,2]-bnds[1][2])<1e-14)
+
+
+####################################
+#           JAX VERSION
+####################################
+
+
+
+def z1_jnp(p):
+    c=jnp.cos(jnp.pi*p[...,1])
+    s=jnp.sin(jnp.pi*p[...,1])
+    c2 = jnp.multiply(c,c)
+    cs = jnp.multiply(c,s)
+    q = jnp.multiply(c2,p[...,0])-jnp.multiply(cs,p[...,2])+c*(R+1)
+    return q
+
+def z2_jnp(p):
+    c=jnp.cos(jnp.pi*p[...,1])
+    s=jnp.sin(jnp.pi*p[...,1])
+    s2 = jnp.multiply(s,s)
+    cs = jnp.multiply(c,s)
+    q = jnp.multiply(cs,p[...,0])-jnp.multiply(s2,p[...,2])+s*(R+1)
+    return q
+def z3_jnp(p):
+    c=jnp.cos(jnp.pi*p[...,1])
+    s=jnp.sin(jnp.pi*p[...,1])
+    q = jnp.multiply(s,p[...,0])+jnp.multiply(c,p[...,2])
+    return q
+
+
+def y1_jnp(p):
+    # p is a vector of points, Nx3
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c=jnp.cos(th)
+    s=jnp.sin(th)
+    c2 = jnp.multiply(c,c)
+    cs = jnp.multiply(c,s)
+    q = jnp.multiply(p[...,0],c2)+jnp.multiply(p[...,1],cs)-(R+1)*c + jnp.multiply(s,p[...,2])
+    return q
+
+def y2_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    return th/jnp.pi
+
+def y3_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c=jnp.cos(th)
+    s=jnp.sin(th)
+    s2 = jnp.multiply(s,s)
+    cs = jnp.multiply(c,s)
+    q = -jnp.multiply(p[...,0],cs)-jnp.multiply(p[...,1],s2)+(R+1)*s+jnp.multiply(c,p[...,2])
+    return q
+
+def y1_d1_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c2t = jnp.cos(2*th)
+    s2t = jnp.sin(2*th)
+    s   = jnp.sin(th)
+    c   = jnp.cos(th)
+    r2  = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    A   = (s2t*p[...,0]*p[...,1] - c2t*p[...,1]*p[...,1] - (R+1)*s*p[...,1] - p[...,2]*p[...,1]*c)
+    return (c2t+1)/2. + A/r2
+
+def y1_d2_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c2t = jnp.cos(2*th)
+    s2t = jnp.sin(2*th)
+    s   = jnp.sin(th)
+    c   = jnp.cos(th)
+    r2  = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    A   = ( c2t*p[...,0]*p[...,1] - s2t*p[...,0]*p[...,0] + (R+1)*s*p[...,0] + p[...,2]*p[...,0]*c)
+    return s2t/2. +A/r2
+
+def y1_d3_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    return jnp.sin(th)
+
+
+def y2_d1_jnp(p):
+    r2 = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    return -(p[...,1]/r2)/jnp.pi
+
+def y2_d2_jnp(p):
+    r2 = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    return (p[...,0]/r2)/jnp.pi
+
+
+def y3_d1_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c2t = jnp.cos(2*th)
+    s2t = jnp.sin(2*th)
+    s   = jnp.sin(th)
+    c   = jnp.cos(th)
+    r2  = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    A   = (c2t*p[...,0]*p[...,1] + s2t*p[...,1]**2 - (R+1)*c*p[...,1] + p[...,2]*p[...,1]*s)
+    return -s2t/2. + A/r2
+
+def y3_d2_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c2t = jnp.cos(2*th)
+    s2t = jnp.sin(2*th)
+    s   = jnp.sin(th)
+    c   = jnp.cos(th)
+    r2  = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    A   = (s2t*p[...,0]*p[...,1] + c2t*p[...,0]*p[...,0] - (R+1)*c*p[...,0] + p[...,2]*p[...,0]*s)
+    return (c2t-1)/2. - A/r2
+
+def y3_d3_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    return jnp.cos(th)
+
+
+def y1_d1d1_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c2t = jnp.cos(2*th)
+    s2t = jnp.sin(2*th)
+    s   = jnp.sin(th)
+    c   = jnp.cos(th)
+    r2  = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    A   = (s2t*p[...,0]*p[...,1] - c2t*p[...,1]*p[...,1] - (R+1)*s*p[...,1] - p[...,2]*p[...,1]*c)
+    dA = p[...,1]*s2t-(2*c2t*p[...,0]*(p[...,1])**2+2*s2t*(p[...,1]**3)-(R+1)*c*(p[...,1]**2) + p[...,2]*s*(p[...,1]**2) )/r2
+    return (p[...,1]*s2t + dA - 2*A*p[...,0]/r2 )/r2
+
+def y1_d2d2_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c2t = jnp.cos(2*th)
+    s2t = jnp.sin(2*th)
+    s   = jnp.sin(th)
+    c   = jnp.cos(th)
+    r2  = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    A   = ( c2t*p[...,0]*p[...,1] - s2t*p[...,0]*p[...,0] + (R+1)*s*p[...,0] + p[...,2]*p[...,0]*c)
+    dA  = p[...,0]*c2t-(2*s2t*p[...,1]*p[...,0]**2+2*c2t*(p[...,0]**3)-(R+1)*c*(p[...,0]**2)+p[...,2]*s*(p[...,0]**2))/r2
+    return (c2t*p[...,0] + dA - 2*A*p[...,1]/r2 )/r2
+
+def y2_d1d1_jnp(p):
+    r2 = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    return (2*p[...,1]*p[...,0]/r2)/(r2*jnp.pi)
+
+def y2_d2d2_jnp(p):
+    r2 = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    return -2*(p[...,0]*p[...,1]/r2)/(r2*jnp.pi)
+
+
+def y3_d1d1_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c2t = jnp.cos(2*th)
+    s2t = jnp.sin(2*th)
+    s   = jnp.sin(th)
+    c   = jnp.cos(th)
+    r2  = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    A   = (c2t*p[...,0]*p[...,1] + s2t*p[...,1]**2 - (R+1)*c*p[...,1] + p[...,2]*p[...,1]*s)
+    dA  = p[...,1]*c2t+(2*p[...,0]*(p[...,1]**2)*s2t-2*(p[...,1]**3)*c2t-(R+1)*(p[...,1]**2)*s-p[...,2]*(p[...,1]**2)*c)/r2
+    return (c2t*p[...,1] + dA - 2*p[...,0]*A/r2)/r2
+
+def y3_d2d2_jnp(p):
+    th = jnp.arctan2(p[...,1],p[...,0])
+    c2t = jnp.cos(2*th)
+    s2t = jnp.sin(2*th)
+    s   = jnp.sin(th)
+    c   = jnp.cos(th)
+    r2  = p[...,0]*p[...,0]+p[...,1]*p[...,1]
+    A   = (s2t*p[...,0]*p[...,1] + c2t*p[...,0]*p[...,0] - (R+1)*c*p[...,0] + p[...,2]*p[...,0]*s)
+    dA  = p[...,0]*s2t+(2*(p[...,0]**2)*p[...,1]*c2t-2*(p[...,0]**3)*s2t + (R+1)*(p[...,0]**2)*s + p[...,2]*(p[...,0]**2)*c)/r2
+    return -(s2t*p[...,0] + dA/r2 - 2*p[...,1]*A/r2)/r2
+
+def gb_jnp(p):
+    return ((jnp.abs(p[...,1]-bnds[0][1]))<1e-14) | ((jnp.abs(p[...,1]-bnds[1][1]))<1e-14) | (jnp.abs(p[...,2]-bnds[0][2])<1e-14) | (jnp.abs(p[...,2]-bnds[1][2])<1e-14)
+
+####################################
+#           OVERALL
+####################################
+
+
+def z1(p,jax_avail = True):
+    if jax_avail:
+        return z1_jnp(p)
+    else:
+        return z1_np(p)
+def z2(p,jax_avail = True):
+    if jax_avail:
+        return z2_jnp(p)
+    else:
+        return z2_np(p)
+def z3(p,jax_avail = True):
+    if jax_avail:
+        return z3_jnp(p)
+    else:
+        return z3_np(p)
+    
+def y1(p,jax_avail = True):
+    if jax_avail:
+        return y1_jnp(p)
+    else:
+        return y1_np(p)
+def y2(p,jax_avail = True):
+    if jax_avail:
+        return y2_jnp(p)
+    else:
+        return y2_np(p)
+def y3(p,jax_avail = True):
+    if jax_avail:
+        return y3_jnp(p)
+    else:
+        return y3_np(p)
+    
+def y1_d1(p,jax_avail = True):
+    if jax_avail:
+        return y1_d1_jnp(p)
+    else:
+        return y1_d1_np(p)
+def y1_d2(p,jax_avail = True):
+    if jax_avail:
+        return y1_d2_jnp(p)
+    else:
+        return y1_d2_np(p)
+def y1_d3(p,jax_avail = True):
+    if jax_avail:
+        return y1_d3_jnp(p)
+    else:
+        return y1_d3_np(p)
+    
+
+def y2_d1(p,jax_avail = True):
+    if jax_avail:
+        return y2_d1_jnp(p)
+    else:
+        return y2_d1_np(p)
+def y2_d2(p,jax_avail = True):
+    if jax_avail:
+        return y2_d2_jnp(p)
+    else:
+        return y2_d2_np(p)
+
+def y3_d1(p,jax_avail = True):
+    if jax_avail:
+        return y3_d1_jnp(p)
+    else:
+        return y3_d1_np(p)
+def y3_d2(p,jax_avail = True):
+    if jax_avail:
+        return y3_d2_jnp(p)
+    else:
+        return y3_d2_np(p)
+def y3_d3(p,jax_avail = True):
+    if jax_avail:
+        return y3_d3_jnp(p)
+    else:
+        return y3_d3_np(p)
+    
+def y1_d1d1(p,jax_avail = True):
+    if jax_avail:
+        return y1_d1d1_jnp(p)
+    else:
+        return y1_d1d1_np(p)
+def y1_d2d2(p,jax_avail = True):
+    if jax_avail:
+        return y1_d2d2_jnp(p)
+    else:
+        return y1_d2d2_np(p)
+    
+def y2_d1d1(p,jax_avail = True):
+    if jax_avail:
+        return y2_d1d1_jnp(p)
+    else:
+        return y2_d1d1_np(p)
+def y2_d2d2(p,jax_avail = True):
+    if jax_avail:
+        return y2_d2d2_jnp(p)
+    else:
+        return y2_d2d2_np(p)
+
+def y3_d1d1(p,jax_avail = True):
+    if jax_avail:
+        return y3_d1d1_jnp(p)
+    else:
+        return y3_d1d1_np(p)
+def y3_d2d2(p,jax_avail = True):
+    if jax_avail:
+        return y3_d2d2_jnp(p)
+    else:
+        return y3_d2d2_np(p)
+def gb(p,jax_avail = True):
+    if jax_avail:
+        return gb_jnp(p)
+    else:
+        return gb_np(p)
+
+def box_geom(jax_avail=True):
+    if jax_avail:
+        return jnp.array(bnds)
+    else:
+        return np.array(bnds)
+def param_geom(jax_avail = True):
+    return ParametrizedGeometry3D(
+                        box_geom(jax_avail),\
+                        z1=lambda p:z1(p,jax_avail),z2=lambda p:z2(p,jax_avail),z3=lambda p:z3(p,jax_avail),\
+                        y1=lambda p:y1(p,jax_avail),y2=lambda p:y2(p,jax_avail),y3=lambda p:y3(p,jax_avail),\
+                        y1_d1=lambda p:y1_d1(p,jax_avail), y1_d2=lambda p:y1_d2(p,jax_avail),y1_d3=lambda p:y1_d3(p,jax_avail),\
+                        y2_d1=lambda p:y2_d1(p,jax_avail), y2_d2=lambda p:y2_d2(p,jax_avail),\
+                        y3_d1=lambda p:y3_d1(p,jax_avail), y3_d2=lambda p:y3_d2(p,jax_avail),y3_d3=lambda p:y3_d3(p,jax_avail),\
+                        y1_d1d1=lambda p:y1_d1d1(p,jax_avail), y1_d2d2=lambda p:y1_d2d2(p,jax_avail),\
+                        y2_d1d1=lambda p:y2_d1d1(p,jax_avail), y2_d2d2=lambda p:y2_d2d2(p,jax_avail),\
+                        y3_d1d1=lambda p:y3_d1d1(p,jax_avail), y3_d2d2=lambda p:y3_d2d2(p,jax_avail)
+                        )
