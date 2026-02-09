02549bbc639e3d86f62004dce5cf8137e4b9a47a
jlk9 <josek97@utexas.edu>
2026-02-09 17:18:11 -0600

    Re-adding solve infrastructure and tests after messed up merge

--- multislab/omsdirectsolve.py
+++ multislab/omsdirectsolve.py
@@ -159,3 +159,176 @@ def block_cyclic_tridiagonal_solve(OMS, T, smw_block, d):
     x = y - smw_block @ y
 
     return x
+
+# Construct a solver for the red-black scheme. This one is not periodic (aka not cyclical)
+def build_block_RB_solver(OMS, S_rk_list, rhs_list, Ntot, nc, cyclic=False):
+    """
+    
+    [  I   ] [ S_12 ] [  0   ] [  E?  ]
+    [ S_21 ] [  I   ] [ S_23 ] [  0   ]
+    [  0   ] [ S_32 ] [  I   ] [ S_34 ]
+    [  F?  ] [  0   ] [ S_43 ] [  I   ]
+
+    Using linear operators corresponding to the slabs of a slab solver, we will construct a block tridiagonal direct solver
+
+    This is based off of the red-black algorithm. ASSUME POWER OF 2 FOR SLABCOUNT ONLY
+
+    We need to store 4 objects:
+    1. Original S_rk_list, all entries are needed for either even solves or RHS of odd solves
+    2. For i odd, the factorized systems B_i' = (I - S_{i,i-1} S_{i-1,i} - S_{i,i+1} S_{i+1,i})^-1 that makes up the "main diagonal"
+    3. For i odd, the factorized system  A_i' = (B_i') \ S_{i,i-1} S_{i-1,i-2}
+    4. For i odd, the factorized system  C_i' = (B_i') \ S_{i,i+1} S_{i+1,i+2}
+    """
+    m      = S_rk_list[0][0].shape[0]
+    nSlabs = len(S_rk_list)
+
+    if not ((nSlabs & (nSlabs-1) == 0) and nSlabs != 0):
+        ValueError("ERROR! Number of slabs is not a power of 2.")
+
+    SiM = [-_[0] for _ in S_rk_list]
+    SiP = [-_[-1] for _ in S_rk_list]
+
+    if not cyclic:
+        def zero_operator(m, dtype=float):
+            """
+            Return a SciPy LinearOperator acting on R^m → R^m that always returns zero.
+            Supports matvec, rmatvec, matmat, rmatmat.
+            """
+            def matvec(x):
+                # x is shape (m,)
+                return np.zeros_like(x)
+
+            def rmatvec(x):
+                # x is shape (m,)
+                return np.zeros_like(x)
+
+            def matmat(X):
+                # X is shape (m, k)
+                return np.zeros_like(X)
+
+            def rmatmat(X):
+                # X is shape (m, k)
+                return np.zeros_like(X)
+
+            return LinearOperator(
+                shape=(m, m),
+                matvec=matvec,
+                rmatvec=rmatvec,
+                matmat=matmat,
+                rmatmat=rmatmat,
+                dtype=dtype,
+            )
+        SiM[0] = zero_operator
+        SiP[-1] = zero_operator
+
+    RB = [(SiM, [np.eye(m, dtype=SiM[0].dtype) for _ in range(nSlabs)], SiP)]
+
+    l = nSlabs
+    while l > 1:
+        RB.append(build_block_RB_solver_level(m, l, RB[-1]))
+        l = int(l / 2)
+
+    # Set up S:
+    (A_i, _, C_i) = RB[-1]
+    S = np.eye(m, dtype=A_i[0].dtype)
+    S -= A_i[0]
+    S -= C_i[0]
+
+    S = lu_factor(S, overwrite_a=True)
+
+    return (RB, S)
+
+
+# Construct a solver for the red-black scheme. This one is not periodic (aka not cyclical)
+def build_block_RB_solver_level(m, nSlabs, RB_level):
+    """
+    
+    [  I   ] [ S_12 ] [  0   ] [  E?  ]
+    [ S_21 ] [  I   ] [ S_23 ] [  0   ]
+    [  0   ] [ S_32 ] [  I   ] [ S_34 ]
+    [  F?  ] [  0   ] [ S_43 ] [  I   ]
+
+    Using linear operators corresponding to the slabs of a slab solver, we will construct a block tridiagonal direct solver
+
+    This is based off of the red-black algorithm.
+
+    We need to store 4 objects:
+    1. Original S_rk_list, all entries are needed for either even solves or RHS of odd solves
+    2. For i odd, the factorized systems B_i' = (I - S_{i,i-1} S_{i-1,i} - S_{i,i+1} S_{i+1,i})^-1 that makes up the "main diagonal"
+    3. For i odd, the factorized system  A_i' = (B_i') \ S_{i,i-1} S_{i-1,i-2}
+    4. For i odd, the factorized system  C_i' = (B_i') \ S_{i,i+1} S_{i+1,i+2}
+    """
+
+    SiM = RB_level[0]
+    SiP = RB_level[2]
+
+    # First let's build 2, B_i:
+    I   = np.eye(m, dtype=SiM[0].dtype)
+    B_i = [I for _ in range(0, nSlabs, 2)]
+    for i in range(0, nSlabs, 2):
+        j = int(i/2)
+        B_i[j] = B_i[j] - SiM[i] @ SiP[(i-1) % nSlabs] @ I - SiP[i] @ SiM[(i+1) % nSlabs] @ I
+
+    # Now we factorize every B_i:
+    B_i = [lu_factor(_, overwrite_a=True) for _ in B_i]
+
+    # Next let's build 3, A_i:
+    A_i = [lu_solve(B_i[int(_ / 2)], SiM[_] @ SiM[(_ - 1) % nSlabs] @ I) for _ in range(0, nSlabs, 2)]
+    # And finally build 4, C_i:
+    C_i = [lu_solve(B_i[int(_ / 2)], SiP[_] @ SiP[(_ + 1) % nSlabs] @ I) for _ in range(0, nSlabs, 2)]
+
+    return (A_i, B_i, C_i)
+
+
+def block_RB_solve(RBS, v):
+    """
+    [  I   ] [ S_12 ] [  0   ] [  E?  ]
+    [ S_21 ] [  I   ] [ S_23 ] [  0   ]
+    [  0   ] [ S_32 ] [  I   ] [ S_34 ]
+    [  F?  ] [  0   ] [ S_43 ] [  I   ]
+
+    Solves using the Red-Black factorization. Assumes we have RB = (A_i, B_i, C_i) and v of size m * nSlabs
+    """
+    (RB, S) = RBS
+
+    m = RB[0][0][0].shape[0]
+    # Building the RHS:
+    vPrimes = [v.copy()]
+    Smats   = []
+
+    # Now build the RHS:
+    for l in range(len(RB) - 1):
+        (SiM, _, SiP)   = RB[l]
+        (_, B_i, _) = RB[l+1]
+
+        nSlabs   = len(SiM)
+        nReduced = int(nSlabs / 2)
+        vPrime   = np.zeros(m*nReduced)
+        vPrev    = vPrimes[-1]
+
+        for j in range(nReduced):
+            i    = 2 * j
+            prev = (i - 1) % nSlabs
+            next = (i + 1) % nSlabs
+            vPrime[j*m:(j+1)*m] = vPrev[i*m:(i+1)*m] + SiM[i] @ vPrev[prev*m:(prev+1)*m] + SiP[i] @ vPrev[next*m:(next+1)*m]
+            vPrime[j*m:(j+1)*m] = lu_solve(B_i[j], vPrime[j*m:(j+1)*m])
+
+        vPrimes.append(vPrime)
+
+    # Now get u:
+    vPrimes[-1] = lu_solve(S, vPrimes[-1])
+    
+    for l in range(len(RB) - 1, 0, -1):
+        (SiM, _, SiP)   = RB[l-1]
+        nReduced = int(len(SiM) / 2)
+        print("l, nReduced:", l, nReduced)
+        for j in range(nReduced):
+            i = 2 * j
+            # We fill in the odd segments of u
+            vPrimes[l-1][i*m:(i+1)*m] = vPrimes[l][j*m:(j+1)*m]
+            
+            # Here we compute the even segments of u:
+            next = (j + 1) % nReduced
+            vPrimes[l-1][(i+1)*m:(i+2)*m] += SiM[i+1] @ vPrimes[l][j*m:(j+1)*m] + SiP[i+1] @ vPrimes[l][next*m:(next+1)*m]
+
+    return vPrimes[0]
