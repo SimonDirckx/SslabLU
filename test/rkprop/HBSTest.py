89a58291340ee2e7f9e19ee6a270ba745af404ce
SimonDirckx <simon.dirckx@hotmail.com>
2026-02-16 16:32:42 -0600

    testing HBSMAT ...

--- test/rkprop/HBSTest.py
+++ test/rkprop/HBSTest.py
@@ -13,6 +13,7 @@ import solver.hpsmultidomain.hpsmultidomain.geom as hpsaltGeom
 import SOMS3D as SOMS
 import matAssembly.matAssembler as mA
 import solver.solver as solver
+import scipy.linalg as sclinalg
 def compute_c0_L0(XX):
     """
     compute the center and charasteristic length of the domain to be clustered
@@ -31,7 +32,7 @@ k = 2
 Lx = 1/8
 Ly = 1
 Lz = 1
-kh = 20
+kh = 2.
 def  c11(p):
     return torch.ones_like(p[:,0])
 def  c22(p):
@@ -53,15 +54,14 @@ ay = .5*(bnds[1,1]/nby)
 az = .5*(bnds[1,2]/nbz)
 
 #isotropic disc
-py=7
-px=7
-pz=7
+py=6
+px=6
+pz=6
 
 solver_hps = hpsalt.Domain_Driver(Om, HH, kh, np.array([ax,ay,az]), [px+1,py+1,pz+1], 3)
 solver_hps.build("reduced_cpu", "MUMPS",verbose=False)
 
-XX = solver_hps.XX
-XXfull = solver_hps.XXfull
+XX = (solver_hps.XX).cpu().detach().numpy()
 
 Jb = solver_hps._Jx
 Ji = solver_hps.Ji
@@ -79,9 +79,11 @@ print("Jc size = ",len(Jc))
 print("Jl size = ",len(Jl))
 Aib = np.array(solver_hps.Aix.todense())
 SS = -(solver_hps.solver_Aii@Aib[:,Jl])[Jc,:]
+XXl = XXb[Jl,:]
+XXc = XXi[Jc,:]
+XXb2d = XXb[Jl,1:3]
 
-c0,L0 = compute_c0_L0(XXb[Jl,:])
-tree0 =  tree.BalancedTree(XXb[Jl,:],(py-1)*(pz-1),c0,L0)
+tree0 =  tree.BalancedTree(XXl,(py-1)*(pz-1))
 
 perm = []
 
@@ -98,11 +100,85 @@ print("matvec err = ",np.linalg.norm(u-ucheck)/np.linalg.norm(ucheck))
 
 
 # level 1 approx:
+leaves = tree0.get_leaves()
+N = SS.shape[0]
+Nleaves = len(leaves)
+print("Nleaves = ",Nleaves)
+k = (py-1)*(pz-1)
+nl = k
+Utot = np.zeros(shape=(N,Nleaves*k))
+Vtot = np.zeros(shape=(N,Nleaves*k))
+Dtot = np.zeros(shape=(N,N))
+Ktot = np.zeros(shape = (Nleaves*k,Nleaves*k))
+print("tot MATS made")
+def compute_col_space(M,k):
+    [Q,_,_] = sclinalg.qr(M,mode='economic',pivoting=True)
+    return Q[:,:k]
 
-for ind in range(len(tree0.get_leaves())):
-    inds = np.append(np.arange(0,(ind-1)*(py-1)*(pz-1)),np.arange((ind+1)*(py-1)*(pz-1),Sp.shape[1]))
-    Ssub = Sp[:,inds][ind*(py-1)*(pz-1):(ind+1)*(py-1)*(pz-1),:]
-    [_,s,_] = np.linalg.svd(Ssub)
-    plt.figure(1)
-    plt.semilogy(s)
+for ind in range(Nleaves):
+    inds = np.arange(ind*nl,(ind+1)*nl)
+    indsc = [i for i in range(N) if i not in inds]
+    U = compute_col_space(Sp[:,indsc][inds,:],k)
+    V = compute_col_space(Sp[:,inds][indsc,:].T,k)
+    D = Sp[inds,:][:,inds]-U@(U.T@Sp[inds,:][:,inds]@V)@V.T
+    Utot[ind*nl:(ind+1)*nl,:][:,ind*k:(ind+1)*k] = U
+    Vtot[ind*nl:(ind+1)*nl,:][:,ind*k:(ind+1)*k] = V
+    Dtot[ind*nl:(ind+1)*nl,:][:,ind*nl:(ind+1)*nl] = D
+Ktot = Utot.T@(Sp@Vtot)
+
+# check if rep is accurate
+v = np.random.standard_normal(size=(N,))
+u = Sp@v
+
+uhat = Utot@(Ktot@(Vtot.T@v)) + Dtot@v
+
+print("Err rep = ",np.linalg.norm(uhat-u)/np.linalg.norm(u))
+
+# level 2 approx:
+Nb = len(tree0.get_boxes_level(tree0.nlevels-2))
+print("Nb = ",Nb)
+print("Ktot shape = ",Ktot.shape)
+
+#k = 2*k
+
+Utot2 = np.zeros(shape=(Nleaves*k,(Nleaves//2)*k))
+Vtot2 = np.zeros(shape=(Nleaves*k,(Nleaves//2)*k))
+Dtot2 = np.zeros(shape=(Nleaves*k,Nleaves*k))
+
+
+XXlperm = XXl[perm,:]
+XXcperm = XXc[perm,:]
+
+for ind in range(Nleaves//2):
+    inds = np.arange(2*ind*k,2*(ind+1)*k)
+    indsc = [i for i in range(Ktot.shape[0]) if i not in inds]
     plt.show()
+    U = compute_col_space(Ktot[:,indsc][inds,:],k)
+    V = compute_col_space(Ktot[:,inds][indsc,:].T,k)
+    print("U col space err = ",np.linalg.norm(Ktot[:,indsc][inds,:]-U@(U.T@Ktot[:,indsc][inds,:])))
+    print("V col space err = ",np.linalg.norm(Ktot[:,inds][indsc,:].T-V@(V.T@Ktot[:,inds][indsc,:].T)))
+    D = Ktot[inds,:][:,inds]-U@(U.T@Ktot[inds,:][:,inds]@V)@V.T
+    Utot2[2*ind*k:2*(ind+1)*k,:][:,ind*k:(ind+1)*k] = U
+    Vtot2[2*ind*k:2*(ind+1)*k,:][:,ind*k:(ind+1)*k] = V
+    Dtot2[2*ind*k:2*(ind+1)*k,:][:,2*ind*k:2*(ind+1)*k] = D
+Ktot2 = Utot2.T@(Ktot@Vtot2)
+Dtot2test = Dtot2
+Dtot2 = Ktot-Utot2@Ktot2@Vtot2.T
+
+v = np.random.standard_normal(size=(N,))
+u = Sp@v
+Ktothat = Utot2@(Ktot2@Vtot2.T) + Dtot2
+uhat = Utot@(Ktothat@(Vtot.T@v)) + Dtot@v
+
+print("Err rep = ",np.linalg.norm(uhat-u)/np.linalg.norm(u))
+
+
+
+
+
+
+
+
+
+
+
