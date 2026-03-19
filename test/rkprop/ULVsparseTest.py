f4e6613ff7761d707663f7a22fa59149800b9ddd
SimonDirckx <simon.dirckx@hotmail.com>
2026-03-19 11:03:44 -0500

    minor edits

--- test/rkprop/ULVsparseTest.py
+++ test/rkprop/ULVsparseTest.py
@@ -22,69 +22,85 @@ import ULVdense
 import time
 
 nl = 16*16
-L = 7
-N = (4**(L-1))*nl
+Nvec = np.array([2**14,2**16,2**18,2**20],dtype=np.int64)#np.array([2**14,2**16,2**18,2**20],dtype=np.int64)
+t_ULV_vec = np.zeros(shape = Nvec.shape)
+t_solve_vec = np.zeros(shape = Nvec.shape)
+for indN in range(len(Nvec)):
+    N = Nvec[indN]
+    Nleaves = N//nl
+    L = (int)(np.log2(Nleaves))//2 + 1
+    kvec = np.array([32,64,128],dtype=np.int64)
 
-Nleaves = N//nl
-Lcheck = (int)(np.log2(Nleaves))//2 + 1
-print("L-Lcheck = ",L-Lcheck)
-k = 32
-k0 = min(nl,k)
-
-
-Nb = Nleaves
-Nbvec = [Nb]
-Utot1_sparse = np.zeros(shape = (Nb*nl,k0))
-Vtot1_sparse = np.zeros(shape = (Nb*nl,k0))
-Dtot1_sparse = np.zeros(shape = (Nb*nl,nl))
-
-for i in range(Nb):
-    Dtot1_sparse[i*nl:(i+1)*nl,:] = np.random.standard_normal(size = (nl,nl))#Dtot1[i*nl:(i+1)*nl,:][:,i*nl:(i+1)*nl].copy()
-    Utot1_sparse[i*nl:(i+1)*nl,:] = sclinalg.orth(np.random.standard_normal(size = (nl,k0)))#Utot1[i*nl:(i+1)*nl,:][:,i*k0:(i+1)*k0].copy()
-    Vtot1_sparse[i*nl:(i+1)*nl,:] = sclinalg.orth(np.random.standard_normal(size = (nl,k0)))#Vtot1[i*nl:(i+1)*nl,:][:,i*k0:(i+1)*k0].copy()
-Umats = [Utot1_sparse]
-Vmats = [Vtot1_sparse]
-Dmats = [Dtot1_sparse]
+    k = 32
+    k0 = min(nl,k)
 
 
-for ind in range(1,L):
-    Nb      = Nb//4
-    Nbvec+=[Nb]
-    if ind==1:
-        n = 4*k0
-    else:
-        n = 4*k
-    Utot_sparse = np.zeros(shape = (Nb*n,k))
-    Vtot_sparse = np.zeros(shape = (Nb*n,k))
-    Dtot_sparse = np.zeros(shape = (Nb*n,n))
+    Nb = Nleaves
+    Nbvec = [Nb]
+    Utot1_sparse = np.zeros(shape = (Nb*nl,k0))
+    Vtot1_sparse = np.zeros(shape = (Nb*nl,k0))
+    Dtot1_sparse = np.zeros(shape = (Nb*nl,nl))
 
     for i in range(Nb):
-        Dtot_sparse[i*n:(i+1)*n,:] = np.random.standard_normal(size = (n,n))#Dtot2[i*n:(i+1)*n,:][:,i*n:(i+1)*n].copy()
-        Utot_sparse[i*n:(i+1)*n,:] = sclinalg.orth(np.random.standard_normal(size = (n,k)))#Utot2[i*n:(i+1)*n,:][:,i*k:(i+1)*k].copy()
-        Vtot_sparse[i*n:(i+1)*n,:] = sclinalg.orth(np.random.standard_normal(size = (n,k)))#Vtot2[i*n:(i+1)*n,:][:,i*k:(i+1)*k].copy()
-    if ind<L-1:
-        Umats += [Utot_sparse]
-        Vmats += [Vtot_sparse]
-    Dmats += [Dtot_sparse]
+        Dtot1_sparse[i*nl:(i+1)*nl,:] = np.random.standard_normal(size = (nl,nl))#Dtot1[i*nl:(i+1)*nl,:][:,i*nl:(i+1)*nl].copy()
+        Utot1_sparse[i*nl:(i+1)*nl,:] = sclinalg.orth(np.random.standard_normal(size = (nl,k0)))#Utot1[i*nl:(i+1)*nl,:][:,i*k0:(i+1)*k0].copy()
+        Vtot1_sparse[i*nl:(i+1)*nl,:] = sclinalg.orth(np.random.standard_normal(size = (nl,k0)))#Vtot1[i*nl:(i+1)*nl,:][:,i*k0:(i+1)*k0].copy()
+    Umats = [Utot1_sparse]
+    Vmats = [Vtot1_sparse]
+    Dmats = [Dtot1_sparse]
 
 
-SHBS0 = HBSnew.HBSMAT()
-SHBS0.set_mats(Umats,Dmats,Vmats,Nbvec)
-ticULV = time.time()
-Q1list,Q2list,W1list,W2list,Uulist,Rlist,R_off_list,NNvec = ULVsparse.compute_ULV(Umats,Dmats,Vmats,Nbvec)
-tocULV = time.time()
+    for ind in range(1,L):
+        Nb      = Nb//4
+        Nbvec+=[Nb]
+        if ind==1:
+            n = 4*k0
+        else:
+            n = 4*k
+        Utot_sparse = np.zeros(shape = (Nb*n,k))
+        Vtot_sparse = np.zeros(shape = (Nb*n,k))
+        Dtot_sparse = np.zeros(shape = (Nb*n,n))
+
+        for i in range(Nb):
+            Dtot_sparse[i*n:(i+1)*n,:] = np.random.standard_normal(size = (n,n))#Dtot2[i*n:(i+1)*n,:][:,i*n:(i+1)*n].copy()
+            Utot_sparse[i*n:(i+1)*n,:] = sclinalg.orth(np.random.standard_normal(size = (n,k)))#Utot2[i*n:(i+1)*n,:][:,i*k:(i+1)*k].copy()
+            Vtot_sparse[i*n:(i+1)*n,:] = sclinalg.orth(np.random.standard_normal(size = (n,k)))#Vtot2[i*n:(i+1)*n,:][:,i*k:(i+1)*k].copy()
+        if ind<L-1:
+            Umats += [Utot_sparse]
+            Vmats += [Vtot_sparse]
+        Dmats += [Dtot_sparse]
 
 
-SHBS = HBSnew.HBSMAT()
-SHBS.set_mats(Umats,Dmats,Vmats,Nbvec)
-x= np.random.standard_normal(size=(SHBS.shape[1],2))
-b = SHBS.matvec(x)
+    SHBS0 = HBSnew.HBSMAT()
+    SHBS0.set_mats(Umats,Dmats,Vmats,Nbvec)
+    ticULV = time.time()
+    Q1list,Q2list,W1list,W2list,Uulist,Rlist,R_off_list,NNvec = ULVsparse.compute_ULV(Umats,Dmats,Vmats,Nbvec)
+    tocULV = time.time()
 
-ticSolveULV = time.time()
-xhat = ULVsparse.solve(Umats,Dmats,Q1list,Q2list,W1list,W2list,Uulist,Rlist,R_off_list,NNvec,Nbvec,b)
-tocSolveULV = time.time()
 
-print("solve err = ",np.linalg.norm(xhat-x)/np.linalg.norm(x))
-print("solve ULV time = ", tocSolveULV-ticSolveULV)
-print("ULV fact. time = ", tocULV-ticULV)
-print("Ndofs = ", SHBS.shape[0])
+    SHBS = HBSnew.HBSMAT()
+    SHBS.set_mats(Umats,Dmats,Vmats,Nbvec)
+    x= np.random.standard_normal(size=(SHBS.shape[1],2))
+    b = SHBS.matvec(x)
+
+    ticSolveULV = time.time()
+    xhat = ULVsparse.solve(Umats,Dmats,Q1list,Q2list,W1list,W2list,Uulist,Rlist,R_off_list,NNvec,Nbvec,b)
+    tocSolveULV = time.time()
+    t_ULV_vec[indN] = tocULV-ticULV
+    t_solve_vec[indN] = tocSolveULV-ticSolveULV
+    print("solve err = ",np.linalg.norm(xhat-x)/np.linalg.norm(x))
+    print("solve ULV time = ", t_solve_vec[indN])
+    print("ULV fact. time = ", t_ULV_vec[indN])
+    print("Ndofs = ", SHBS.shape[0])
+
+plt.figure(1)
+plt.loglog(Nvec,t_ULV_vec)
+plt.loglog(Nvec,Nvec*1.1*(t_ULV_vec[0]/Nvec[0]),linestyle='dashed')
+plt.legend(['tULV','O(N)'])
+
+plt.figure(2)
+plt.loglog(Nvec,t_solve_vec)
+plt.loglog(Nvec,Nvec*1.1*(t_solve_vec[0]/Nvec[0]),linestyle='dashed')
+plt.legend(['tsol','O(N)'])
+
+plt.show()
